5e69c6164960af880c41a54faaf5852bce86755c
Trevor Brixey <t.brixey0129@gmail.com>
2018-07-19 21:17:04 -0500

    🐛fixed auth test

--- src/components/authentication/Authentication.test.js
+++ src/components/authentication/Authentication.test.js
@@ -4,6 +4,6 @@ import Authentication from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Authentication />, Authentication);
-  ReactDOM.unmountComponentAtNode(Authentication);
+  ReactDOM.render(<Authentication />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
